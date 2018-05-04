98fd2421c0cc9c40ca52f6ed24b74c15a2b718b9
sadeghdr <36894141+sadeghdr@users.noreply.github.com>
2018-05-04 14:12:48 +0000

    Update assignment4.py

--- assignment4.py
+++ assignment4.py
@@ -5,7 +5,17 @@ def port_scanner():
     21-1000 then write the result to a file using
     write_to_file function
     """
-
+    nm = nmap.PortScanner()
+    nm.scan(ports, '21-1000')    
+    print(ports)    
+    print(nm.all_hosts())  
+    for host_names in nm.all_hosts():
+         print(ports)       
+         ports = nm[host_names].get('tcp') 
+         print(ports)       
+         write_to_file(ports)
+    return ports
+    
 def write_to_file(contents):
     """
     first remove the pass from function body
@@ -13,10 +23,23 @@ def write_to_file(contents):
     :param contents: the result that should be written to file
     :return: true if the writing successfull and false if can't
     """
+    
+    f = open('result_of_nmap', 'sadegh4') 
+    f.write(str(contents))
+
     pass
 
 
 def main():
-    port_scanner()
+    hosts = {    
+        'p30download.com',  
+        'google.com',    
+        'downloadha.com', 
+        'iaun.ac.ir',   
+        'maktabkhooneh.org',  
+        'soft98.ir',
+    }
+      for host_name in hosts:    
+        port_scanner(host_name)
 
 main()
